174df3e9fb9f610d6fa2f618f003031669d01b59
Kgnot <hricaurtem@proton.me>
2025-09-15 14:52:09 -0500

    Arreglo del apartado de prueba de virtual threads xd

--- src/main/java/org/server/Main.java
+++ src/main/java/org/server/Main.java
@@ -23,30 +23,30 @@ public class Main {
             e.printStackTrace();
         }
 
-        try {
-            DataBasesConnection dbc = new DataBasesConnection();
-            UserDao userDao = new UserDao(dbc);
-
-            // --- PRIMERA FORMA: directa ---
-            long start1 = System.nanoTime();
-            var result = userDao.getUsers();
-            long end1 = System.nanoTime();
-            System.out.println("Directo: " + result);
-            System.out.println("Tiempo directo: " + (end1 - start1) / 1_000_000.0 + " ms");
-
-            // --- SEGUNDA FORMA: con UserService (futuro) ---
-            UserService userService = new UserService();
-
-            long start2 = System.nanoTime();
-            CompletableFuture<List<User>> result2 = userService.getUsers();
-            List<User> users = result2.get(); // bloquea hasta que termina
-            long end2 = System.nanoTime();
-
-            System.out.println("Con Future: " + users);
-            System.out.println("Tiempo con Future: " + (end2 - start2) / 1_000_000.0 + " ms");
-
-        } catch (SQLException | ExecutionException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            DataBasesConnection dbc = new DataBasesConnection();
+//            UserDao userDao = new UserDao(dbc);
+//
+//            // --- PRIMERA FORMA: directa ---
+//            long start1 = System.nanoTime();
+//            var result = userDao.getUsers();
+//            long end1 = System.nanoTime();
+//            System.out.println("Directo: " + result);
+//            System.out.println("Tiempo directo: " + (end1 - start1) / 1_000_000.0 + " ms");
+//
+//            // --- SEGUNDA FORMA: con UserService (futuro) ---
+//            UserService userService = new UserService();
+//
+//            long start2 = System.nanoTime();
+//            CompletableFuture<List<User>> result2 = userService.getUsers();
+//            List<User> users = result2.get(); // bloquea hasta que termina
+//            long end2 = System.nanoTime();
+//
+//            System.out.println("Con Future: " + users);
+//            System.out.println("Tiempo con Future: " + (end2 - start2) / 1_000_000.0 + " ms");
+//
+//        } catch (SQLException | ExecutionException | InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
